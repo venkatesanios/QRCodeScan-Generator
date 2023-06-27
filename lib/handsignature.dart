90df9235b39e5d4193684762a2073ff1596d124b
venkatesanios <venkatesan.niagara@gmail.com>
2023-06-27 14:39:19 +0530

    added loccation in signature .png

--- lib/handsignature.dart
+++ lib/handsignature.dart
@@ -2,15 +2,19 @@ import 'dart:io';
 import 'dart:ui' as ui;
 import 'dart:typed_data';
 import 'dart:ui';
+import 'package:geolocator/geolocator.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hand_signature/signature.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:qrcodeprint/Permission.dart';
 import 'package:share/share.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:open_file/open_file.dart';
+import 'package:geolocator/geolocator.dart';
 
 HandSignatureControl control = HandSignatureControl(
   threshold: 5.0,
@@ -74,6 +78,9 @@ class _HandsignmyAppState extends State<HandsignmyApp> {
                     ),
                     CupertinoButton(
                       onPressed: () async {
+                        PermissionClass permission = PermissionClass();
+                        permission.checkPermission(
+                            context, Permission.location);
                         rawImageFit.value = await control.toImage(
                           color: Colors.black,
                           background: Colors.lightGreenAccent[100],
@@ -126,62 +133,6 @@ class _HandsignmyAppState extends State<HandsignmyApp> {
       final buffer = byteData.buffer;
       final tempDir = await getTemporaryDirectory();
       final filePath = '${tempDir.path}/signature.png';
-      File(filePath).writeAsBytesSync(buffer.asUint8List());
-      print(filePath);
-
-      setState(() {
-        downloadPath = filePath;
-      });
-
-      _shareImage(filePath);
-      // ScaffoldMessenger.of(context).showSnackBar(
-      //   SnackBar(content: Text('Image saved at $filePath')),
-      // );
-    } catch (e) {
-      print('Error saving image: $e');
-      // Show an error message if the image couldn't be saved
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Failed to save image')),
-      );
-    }
-  }
-
-  void _saveImage2(BuildContext context, ByteData? byteData) async {
-    if (byteData == null) {
-      return;
-    }
-
-    try {
-      final buffer = byteData.buffer;
-      final tempDir = await getTemporaryDirectory();
-      final filePath = '${tempDir.path}/signature.png';
-      await File(filePath).writeAsBytes(buffer.asUint8List());
-
-      openPath(filePath);
-      final result = await ImageGallerySaver.saveFile(filePath);
-      print('Image saved to gallery: $result');
-
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Image saved to gallery')),
-      );
-    } catch (e) {
-      print('Error saving image: $e');
-
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Failed to save image')),
-      );
-    }
-  }
-
-  void _saveImage(BuildContext context, ByteData? byteData) async {
-    if (byteData == null) {
-      return;
-    }
-
-    try {
-      final buffer = byteData.buffer;
-      final tempDir = await getTemporaryDirectory();
-      final filePath = '${tempDir.path}/signature.png';
       await File(filePath).writeAsBytes(buffer.asUint8List());
 
       final imageFile = File(filePath);
@@ -223,6 +174,79 @@ class _HandsignmyAppState extends State<HandsignmyApp> {
 
       openPath(stampedFilePath);
       final result = await ImageGallerySaver.saveFile(stampedFilePath);
+      print('Image saved to gallery: $result');
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Image saved to gallery')),
+      );
+    } catch (e) {
+      print('Error saving image: $e');
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to save image')),
+      );
+    }
+  }
+
+  void _saveImage(BuildContext context, ByteData? byteData) async {
+    if (byteData == null) {
+      return;
+    }
+
+    try {
+      final buffer = byteData.buffer;
+      final tempDir = await getTemporaryDirectory();
+      final filePath = '${tempDir.path}/signature.png';
+      await File(filePath).writeAsBytes(buffer.asUint8List());
+
+      final imageFile = File(filePath);
+      final timeStamp =
+          DateFormat('yyyy-MM-dd/HH-mm-ss').format(DateTime.now());
+      final stampedFilePath = '${tempDir.path}/signature_$timeStamp.png';
+
+      final imageBytes = await imageFile.readAsBytes();
+      final image = await decodeImageFromList(imageBytes);
+
+      final recorder = ui.PictureRecorder();
+      final canvas = Canvas(recorder);
+      final paint = Paint()..color = Colors.black;
+      final textSize = 20.0;
+      final padding = 10.0;
+
+      canvas.drawImage(image, Offset.zero, paint);
+
+      final paragraphStyle = ui.ParagraphStyle(
+        textAlign: TextAlign.left,
+        fontSize: textSize,
+      );
+      final paragraphBuilder = ui.ParagraphBuilder(paragraphStyle);
+      paragraphBuilder.addText(timeStamp);
+
+      // Get current location
+      final position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+      final location = ' ${position.latitude}, ${position.longitude}';
+      paragraphBuilder.addText('\t$location');
+      print('location');
+      print(location);
+
+      final paragraph = paragraphBuilder.build();
+      paragraph.layout(ui.ParagraphConstraints(width: image.width.toDouble()));
+      canvas.drawParagraph(
+        paragraph,
+        Offset(padding, image.height.toDouble() - textSize - padding),
+      );
+
+      final picture = recorder.endRecording();
+      final stampedImage = await picture.toImage(image.width, image.height);
+      final stampedImageData =
+          await stampedImage.toByteData(format: ui.ImageByteFormat.png);
+      final stampedImageBytes = Uint8List.view(stampedImageData!.buffer);
+      await File(filePath).writeAsBytes(stampedImageBytes);
+
+      openPath(filePath);
+      final result = await ImageGallerySaver.saveFile(filePath);
       print('Image saved to gallery: $result');
 
       ScaffoldMessenger.of(context).showSnackBar(
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -8,6 +8,7 @@
 
 #include <file_selector_windows/file_selector_windows.h>
 #include <geolocator_windows/geolocator_windows.h>
+#include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <printing/printing_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
@@ -15,6 +16,8 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
   GeolocatorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("GeolocatorWindows"));
+  PermissionHandlerWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
   PrintingPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PrintingPlugin"));
 }
